05a23893f3e72498725334d9a8989f6808568494
Andrew Burnie <apburnie@hotmail.co.uk>
2016-02-22 17:15:03 +0000

    calculates tax amount

--- app/cost.py
+++ app/cost.py
@@ -1,9 +1,11 @@
 from app.interpreter import Interpreter
 
+
 class Cost(object):
     """Calculates the costs associated with an order"""
     def __init__(self, interpreter=Interpreter()):
         self.interpreter = interpreter
+        self._TAX = 0.0864
 
     def pretax(self, order):
         total = []
@@ -11,8 +13,9 @@ class Cost(object):
             total.append(self.price(item) * order[item])
         return reduce(self.add, total)
 
-    def tax(self, arg):
-        pass
+    def tax(self, order):
+        tax = (self.pretax(order))*self._TAX
+        return round(tax, 2)
 
     def price(self, item):
         return self.interpreter.price(item)
--- tests/test_cost.py
+++ tests/test_cost.py
@@ -16,7 +16,12 @@ class TestCost(unittest.TestCase):
     def test_pretax_gives_pretax_amount(self):
         self.assertEqual(self.cost.pretax({"Cafe Latte": 2,
                                             "Blueberry Muffin": 1, "Choc Mudcake": 1}),
-                                            round((2*4.75 + 4.05 + 6.40), 2))
+                                            round(self.sample_cost(), 2))
+
+    def test_tax_gives_tax_amount(self):
+        self.assertEqual(self.cost.tax({"Cafe Latte": 2,
+                                            "Blueberry Muffin": 1, "Choc Mudcake": 1}),
+                                            round(self.sample_cost()*0.0864, 2))
 
     def side_effect(self, value):
         if value == "Cafe Latte":
@@ -25,3 +30,8 @@ class TestCost(unittest.TestCase):
             return 4.05
         if value == "Choc Mudcake":
             return 6.40
+
+    def sample_cost(self):
+        return (2*self.interpreter.price("Cafe Latte")
+               + self.interpreter.price("Blueberry Muffin")
+               + self.interpreter.price("Choc Mudcake"))
