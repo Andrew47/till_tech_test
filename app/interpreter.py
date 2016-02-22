575cf61962318560c3691687ff78f2a95620e8ba
Andrew Burnie <apburnie@hotmail.co.uk>
2016-02-22 14:43:26 +0000

    receipt gives restaurant details

--- app/interpreter.py
+++ app/interpreter.py
@@ -9,11 +9,8 @@ class Interpreter(object):
         return self.read_json_file()[0]['address']
 
     def phone(self):
-        pass
+        return self.read_json_file()[0]['phone']
 
     def read_json_file(self):
         with open('hipstercoffee.json') as data_file:
             return json.load(data_file)
-
-    def test(self):
-        print self.read_json_file()[0]['phone']
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -12,3 +12,6 @@ class TestInterpreter(unittest.TestCase):
 
     def test_address_extracted_from_JSON(self):
         self.assertEqual(self.interpreter.address(), "123 Lakeside Way")
+
+    def test_address_extracted_from_JSON(self):
+        self.assertEqual(self.interpreter.phone(), "16503600708")
