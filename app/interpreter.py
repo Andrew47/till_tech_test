b0fa8ddd1e242de2332b0c21c4c74e268469faf0
Andrew Burnie <apburnie@hotmail.co.uk>
2016-02-22 14:37:17 +0000

    Interpreter extracts name from JSON file

--- app/interpreter.py
+++ app/interpreter.py
@@ -1,12 +1,19 @@
-
+import json
 
 class Interpreter(object):
     """Interprets the JSON doc"""
     def shopName(self):
-        return ""
+        return self.read_json_file()[0]['shopName']
 
     def address(self):
         pass
 
     def phone(self):
         pass
+
+    def read_json_file(self):
+        with open('hipstercoffee.json') as data_file:
+            return json.load(data_file)
+
+    def test(self):
+        print self.read_json_file()[0]['phone']
